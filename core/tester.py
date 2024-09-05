3994f2f7a0d8ecaaa0c16abeb8ed9870bb5769b6
chillxvt <chillxvt6@gmail.com>
2024-09-06 00:03:05 +0200

    Updated the core class of the tester to be functional

--- core/tester.py
+++ core/tester.py
@@ -49,7 +49,7 @@ class Tester:
         self.targets = new_targets
 
     # Function to ping a single target
-    def reachable(self, targets, timeout = 0.9):
+    def reachable(self, targets, timeout = 0.5):
         result = True
         with ThreadPoolExecutor() as executor:
             futures = {executor.submit(self.ping_target, target, timeout): target for target in targets}
@@ -63,7 +63,7 @@ class Tester:
 
     def ping_target(self, target, timeout):
         response_time = ping(target, timeout=timeout)
-        if response_time is not None:
+        if response_time is not None and response_time is not False:
             self.logger.debug(f'Target {target} reached. Response time: {response_time} seconds')
             return True
         else:
@@ -71,13 +71,16 @@ class Tester:
             return False
 
     def test(self):
-        self.logger.debug("Testing reachability")
-        if not self.reachable(self.targets) and not self.recording:
-            self.start_recording()
-            self.logger.debug("Recording time...")
-        elif self.recording and self.reachable(self.targets):
+        recording = self.get_recording()
+        reachable = self.reachable(self.targets)
+        if recording and reachable:
             self.stop_recording()
             self.logger.debug("Stopping recording...")
+        elif not reachable and not recording:
+            self.start_recording()
+            self.logger.debug("Recording time...")
+        else:
+            self.logger.debug(f"Business as usual")
 
     def run(self):
         if self.armed:
@@ -89,6 +92,7 @@ class Tester:
 
     def start_recording(self):
         self.t0 = datetime.now()
+        self.recording = True
         self.logger.info(f"Failover test started at: {self.t0.strftime('%Y-%m-%d %H:%M:%S')}")
 
 
